1ccc12b5720acecd3ae0b669f551dfe9e43ae5d2
thekovic <72971433+thekovic@users.noreply.github.com>
2024-11-29 18:21:51 +0100

    system: Add RAM segment definitions

--- kernel/include/system.h
+++ kernel/include/system.h
@@ -10,4 +10,24 @@ extern int __boot_tvtype;
 // Reset type as detected by IPL3.
 extern int __boot_resettype;
 
+// User segment base, TLB mapped.
+#define MEM_KUSEG_BASE (0)
+// Kernel segment base, directly mapped, cached.
+#define MEM_KSEG0_BASE (0x80000000)
+// Kernel segment base, directly mapped, uncached .
+#define MEM_KSEG1_BASE (0xA0000000)
+// Supervisor segment base, TLB mapped.
+#define MEM_KSSEG_BASE (0xC0000000)
+// Kernel segment base, TLB mapped.
+#define MEM_KSEG3_BASE (0xE0000000)
+
+// Turn address in any segment into physical address.
+#define ADDR_TO_PHYS(addr) (addr & 0x1FFFFFFF)
+
+__attribute__((noreturn))
+static inline void abort(void)
+{
+    while(1) {}
+}
+
 #endif
