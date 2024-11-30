4eef03963e7ef85cc88f9517a2bdbdcc7ec55b35
thekovic <72971433+thekovic@users.noreply.github.com>
2024-12-01 00:18:52 +0100

    system: Fix VI regs initialization

--- kernel/src/system.c
+++ kernel/src/system.c
@@ -3,6 +3,7 @@
 #include "mi.h"
 #include "pi.h"
 #include "vi.h"
+#include "interrupt.h"
 
 int __boot_memsize;
 int __boot_random_seed;
@@ -20,20 +21,18 @@ void cop0_status_reset(void)
 
 void vi_init(void)
 {
-    *VI_regs = (VI_registers_t) {
-        .control = (VI_CTRL_TYPE_32_BPP | VI_CTRL_AA_RESAMPLE | VI_CTRL_PIXEL_ADVANCE_DEFAULT),
-        .origin = VI_ORIGIN_SET(0),
-        .width = VI_WIDTH_SET(0),
-        .v_interrupt = VI_V_INTR_SET(2),
-        .v_current = 0,
-        .h_burst = VI_H_BURST_SET(VI_BURST_START_NTSC, VI_VSYNC_WIDTH_NTSC, VI_BURST_WIDTH_NTSC, VI_HSYNC_WIDTH_NTSC),
-        .v_total = VI_V_TOTAL_SET(525),
-        .h_total = VI_H_TOTAL_SET(0, 3093),
-        .h_total_leap = VI_H_TOTAL_LEAP_SET(3093, 3093),
-        .h_video = VI_H_VIDEO_SET(108, 748),
-        .v_video = VI_V_VIDEO_SET(35, 515),
-        .v_burst = VI_V_BURST_SET(14, 516),
-        .x_scale = VI_X_SCALE_SET(0, 640),
-        .y_scale = VI_Y_SCALE_SET(0, 240),
-    };
+    VI_regs->control = (VI_CTRL_TYPE_32_BPP | VI_CTRL_AA_RESAMPLE | VI_CTRL_PIXEL_ADVANCE_DEFAULT);
+    VI_regs->origin = VI_ORIGIN_SET(0);
+    VI_regs->width = VI_WIDTH_SET(320);
+    VI_regs->v_interrupt = VI_V_INTR_SET(2);
+    VI_regs->v_current = 0;
+    VI_regs->h_burst = VI_H_BURST_SET(VI_BURST_START_NTSC, VI_VSYNC_WIDTH_NTSC, VI_BURST_WIDTH_NTSC, VI_HSYNC_WIDTH_NTSC);
+    VI_regs->v_total = VI_V_TOTAL_SET(525);
+    VI_regs->h_total = VI_H_TOTAL_SET(0, 3093);
+    VI_regs->h_total_leap = VI_H_TOTAL_LEAP_SET(3093, 3093);
+    VI_regs->h_video = VI_H_VIDEO_SET(108, 748);
+    VI_regs->v_video = VI_V_VIDEO_SET(35, 515);
+    VI_regs->v_burst = VI_V_BURST_SET(14, 516);
+    VI_regs->x_scale = VI_X_SCALE_SET(320, 640);
+    VI_regs->y_scale = VI_Y_SCALE_SET(240, 240);
 }
